a821070c68007059c06aa4db77d4858980ceb093
Benny Powers <web@bennypowers.com>
2020-08-07 17:28:27 +0300

    feat(navigation): remove a11y warning
    containers with click listeners should be interactive. This PR ensures 
    that only clickable elements receive click listeners.

--- packages/navigation/RocketNavigation.js
+++ packages/navigation/RocketNavigation.js
@@ -1,18 +1,68 @@
+// @ts-check
+
+/**
+ * Is the Node an Element?
+ * @param  {Node}  node
+ * @return {node is Element}
+ */
+function isElement(node) {
+  return node.nodeType === 1;
+}
+
 export class RocketNavigation extends HTMLElement {
   // static get observedAttributes() {
   //   return ['content-node-selector'];
   // }
+
   constructor() {
     super();
     // this.currentNode = null;
     // this.currentNodeSelector = '.current';
     // this.contentNodeSelector = 'main';
-    this.addEventListener('click', ev => {
-      if (ev.target.classList.contains('anchor')) {
-        this.dispatchEvent(new Event('close-overlay', { bubbles: true }));
-      }
-    });
+    this.closeOverlay = this.closeOverlay.bind(this);
+    this.addClickListener = this.addClickListener.bind(this);
+    this.__mo = new MutationObserver(records => {
+      records.forEach(({ addedNodes, removedNodes }) => {
+        addedNodes.forEach(this.addClickListener);
+        removedNodes.forEach(this.removeClickListener);
+      })
+    })
   }
+
+  connectedCallback() {
+    this.setAttribute('role', 'navigation');
+    for (const link of Array.from(this.querySelectorAll('.anchor')))
+      this.addClickListener(link);
+  }
+
+  /**
+   * Adds Click listener to anchor links
+   * @param {Node} node
+   * @private
+   */
+  addClickListener(node) {
+    if (isElement(node) && node.classList.contains('anchor')) {
+      node.addEventListener('click', this.closeOverlay);
+    }
+  }
+
+  /**
+   * Removes Click listener from anchor links
+   * @param {Node} node
+   * @private
+   */
+  removeClickListener(node) {
+    node.removeEventListener('click', this.closeOverlay);
+  }
+
+  /**
+   * Dispatches a `close-overlay` event
+   * @private
+   */
+  closeOverlay() {
+    this.dispatchEvent(new CustomEvent('close-overlay', { bubbles: true }));
+  }
+
   // attributeChangedCallback(name, oldValue, newValue) {
   //   if (name === 'content-node-selector') {
   //     this.contentNodeSelector = newValue;

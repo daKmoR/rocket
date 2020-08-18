bd611050af73ea6ab8fddc6f6653f961ea0bf75c
Thomas Allmer <d4kmor@gmail.com>
2020-08-18 20:04:27 +0200

    chore: remove site.url

--- docs/_data/site.js
+++ docs/_data/site.js
@@ -7,7 +7,6 @@ module.exports = {
   ...defaultValues,
   name: 'Rocket',
   description: 'Rocket is the way to build fast static websites with a sprinkle of javascript',
-  url: 'https://wip-rocket.netlify.app',
   socialLinks: [
     {
       name: 'GitHub',

0ac39bc9345474d9e8573d9b0c9b98b4ace5312e
Thomas Allmer <d4kmor@gmail.com>
2020-09-13 13:08:50 +0200

    chore: cleanup computedConfig file on close

--- packages/cli/test/RocketCli.test.js
+++ packages/cli/test/RocketCli.test.js
@@ -1,18 +1,44 @@
 import chai from 'chai';
+import fs from 'fs';
 import { RocketCli } from '../src/RocketCli.js';
+import computedConfig from '../src/public/computedConfig.cjs';
 
 const { expect } = chai;
 
+const { COMPUTED_CONFIG_PATH } = computedConfig;
+
 describe('RocketCli', () => {
+  let cli;
+
+  afterEach(async () => {
+    if (cli?.cleanup) {
+      await cli.cleanup();
+    }
+  });
+
   it('has the default command help', async () => {
-    const cli = new RocketCli({ argv: [] });
+    cli = new RocketCli({ argv: [] });
     await cli.setup();
     expect(cli.config.command).to.equal('help');
   });
 
   it('does accept a command as the 2nd parameter', async () => {
-    const cli = new RocketCli({ argv: ['build'] });
+    cli = new RocketCli({ argv: ['build'] });
     await cli.setup();
     expect(cli.config.command).to.equal('build');
+  });
+
+  it('does create and cleanup a computed config file', async () => {
+    cli = new RocketCli();
+    // no config when we start
+    expect(fs.existsSync(COMPUTED_CONFIG_PATH)).to.be.false;
+
+    // has a config after setup
+    await cli.setup();
+    expect(fs.existsSync(COMPUTED_CONFIG_PATH)).to.be.true;
+
+    // cleans it up
+    await cli.cleanup();
+    expect(fs.existsSync(COMPUTED_CONFIG_PATH)).to.be.false;
   });
 });

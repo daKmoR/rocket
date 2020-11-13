635e36d810bbee2db246be1149e31dfa79b4373d
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2020-11-13 14:12:32 +0100

    feat(launch): use nunjuks blocks to define sections

--- docs/_data/site.cjs
+++ docs/_data/site.cjs
@@ -1,5 +1,7 @@
 module.exports = async function () {
   return {
+    dir: 'ltr',
+    lang: 'en',
     name: 'Rocket',
     description: 'Rocket is the way to build fast static websites with a sprinkle of javascript',
     socialLinks: [
--- packages/launch/theme/_data/site.cjs
+++ packages/launch/theme/_data/site.cjs
@@ -1,5 +1,7 @@
 module.exports = function () {
   return {
+    dir: 'ltr',
+    lang: 'en',
     name: 'Rocket',
     description: 'Rocket is the way to build fast static websites with a sprinkle of javascript',
     socialLinks: [
